d36d0e93e70d36fb34f06eca2b3c99cccac5dee6
Eliel <elielpdutra.dev@gmail.com>
2024-07-06 18:34:46 -0300

    JS: arrays

---
+++ js/arrays.js
@@ -0,0 +1,18 @@
+const alunos = [
+    {
+        nome: 'Eliel',
+        idade: 22
+    },
+
+    {
+        nome: 'José',
+        idade: 30
+    },
+]
+
+alunos.push({
+    nome: 'Adão',
+    idade: 10
+},)
+
+console.log(alunos[2].idade);
