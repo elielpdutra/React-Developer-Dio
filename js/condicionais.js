002478a18a646fad46cd7c9131497cf9e0ca0bb8
Eliel <elielpdutra.dev@gmail.com>
2024-07-06 18:02:07 -0300

    JS: Condicionais

---
+++ js/condicionais.js
@@ -0,0 +1,25 @@
+// const idade = 25;
+
+// if(idade > 18) {
+//     console.log('Maior que 18');
+// }else {
+//     console.log('Menor que 18');
+// }
+
+ const nota = 7;
+
+nota > 5 ? console.log('aprovado') : console.log('reprovado');
+
+
+// const nota = 9;
+
+// switch (nota) {
+//     case nota < 5:
+//         console.log("Abaixo da média");
+//         break;
+//     case nota > 5:
+//         console.log("Na média");
+//         break;
+//     default:
+//         console.log("Acima da média");
+// }
