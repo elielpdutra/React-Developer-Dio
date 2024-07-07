36b819378267fe7b683dfa4a9f7336a4b2799610
Eliel <elielpdutra.dev@gmail.com>
2024-07-07 04:55:04 -0300

    Preparando o Container da Calculadora

---
+++ DesafioDeProjeto-Criando-uma-calculador-com-react/desafio01-calculadora/src/App.js
@@ -0,0 +1,12 @@
+import { Container, Content } from "./styles";
+const App = () => {
+  return (
+    <Container>
+      <Content>
+        Olá
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
