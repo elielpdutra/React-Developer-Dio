b750b2353ff5c639a38ffb236d12964868bb0f18
Eliel <elielpdutra.dev@gmail.com>
2024-09-30 23:44:02 -0300

    Ciclo de vida

--- Criando-um-projeto-react-do-zero/src/App.js
+++ Criando-um-projeto-react-do-zero/src/App.js
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-    const [usuarios, SetUsuarios] = useState(["Pablo", "José", "Manoel"]);
+    const [usuario, setUsuario] = useState("");
+    const [count, setCount] = useState(0);
+    const [usuarios, setUsuarios] = useState(["Pablo", "José", "Manoel"]);
  
+    const handleAddUser = () => {
+        setUsuarios([...usuarios, usuario]);
+        setUsuario("");
+    }
+
+    useEffect(() => {
+        setCount(usuarios.length);   
+    }, [usuarios]);
+
     return (
         <div className="App">
             <h1>Hello DIO!</h1>
+            <h3>Total: {count}</h3>
+            <div>
+               <input
+                value={usuario}
+                onChange={(event) => setUsuario(event.target.value)}
+               />
+               <button onClick={handleAddUser}>Adicionar</button>
+               
+            </div>
+            <hr />
             {usuarios.map((item) => (
                 <p>{item}</p>
             ))}
+            
         </div>
     )
 };
